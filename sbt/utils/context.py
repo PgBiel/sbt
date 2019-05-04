234e8af184645e30f91ec5a17d196e6510b60637
Riley Shaw <contact@shiney.dev>
2019-05-04 05:30:28 +0100

    update __all__

--- sbt/utils/context.py
+++ sbt/utils/context.py
@@ -23,6 +23,7 @@ __version_info__ = (1, 0, 0, "alpha", 0)
 __version__      = "{0}.{1}.{2}{3}{4}".format(*[str(n)[0] if (i == 3) else str(n) for (i, n) in enumerate(__version_info__)])
 
 __all__ = {
+    "Stream",
     "Suppress",
     "Timer",
 }
